3df536bbb4a42d5588d2301848fc23ec01fe9e69
MoMac <moritz.maciuga@gmail.com>
2016-02-13 19:23:15 +0100

    Added PreloadJS/EaselJS

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
+ window.addEventListener('resize', resize, false);
+ var stage,
+ 	 preload,
+ 	 manifest;
+
  function init() {
-	var stage = new createjs.Stage("demoCanvas");
 
-	var circle = new createjs.Shape();
-	circle.graphics.beginFill("red").drawCircle(0, 0, 50);
-	circle.x = 100;
-	circle.y = 100;
-	stage.addChild(circle);
+	stage = new createjs.Stage("demoCanvas");
 
+	preload = new createjs.LoadQueue(false, 'images/');
+	preload.on('fileload', handleFileLoad);
+	preload.on('error', handleFileError);
+
+	preload.setMaxConnections(5);
+
+	createBackground();
+
+	createjs.Ticker.addEventListener("tick", tick);
+	resize();
+}
+
+function tick() {
 	stage.update();
+}	
+
+
+function resize() {	
+	stage.canvas.width = window.innerWidth;
+	stage.canvas.height = window.innerHeight;
+
+	
+	
+
+}
+
+function createBackground() {
+		bg = new createjs.Bitmap(img);
+		stage.addChild(bg);
+
+	manifest.push({
+		id: "background",
+		src: "images/test-bg.jpg"
+	});
 
 }
