a4425804382847c26fbc9b882f30e3a5cae0f244
MoMac <moritz.maciuga@gmail.com>
2016-02-22 20:10:55 +0100

    Fonts/Hovers/First Click Events

--- js/main.js
+++ js/main.js
@@ -2,6 +2,26 @@ $(window).resize(function () {
 	resizeLayout();
 });
 
+try {
+	WebFont.load({
+	    google: {
+	        families: ['Slabo 27px']
+	    },
+	    active: function () {
+	        window.init();
+	    },
+	    inactive: function() {
+	        console.log("Could not load Google Font, using default.");
+	        window.init();
+	    }
+	});
+}
+catch (e) {
+	console.log("Error initializing WebFont: " + e);
+	window.init();
+}
+
+//Init
  var stage,
  	 preload,
  	 layoutContainer;
@@ -38,22 +58,32 @@ function resizeLayout() {
 	}
 
 	uiElements.headerBar.scaleX = $(window).width();
+}
 
-	// uiElements.headerButton1.x = 150;
-	// uiElements.headerButton2.x = 300;
-	// uiElements.headerButton3.x = 445;
+function preloadAssets (manifest, fileloadCallback, completeCallback) {
+
+	preload = new createjs.LoadQueue(false, 'images/');
+
+	if (fileloadCallback) {
+		preload.on('fileload', fileloadCallback);
+	}
+	if (completeCallback) {
+		preload.on('complete', completeCallback);
+	}
+
+	preload.loadManifest(manifest);
 }
 
 function createBackground() {
-
-	preload = new createjs.LoadQueue(false, 'images/');
-	preload.on('fileload', backgroundFileLoaded);
-	preload.on('complete', backgroundLoaded);
 
 	manifest = [
 		{
 			id: "background",
 			src: "test-bg.jpg"
+		},
+		{
+			id: "contentArea",
+			src: "general-content-bg.jpg"
 		},
 		{
 			id: "headerBar",
@@ -65,31 +95,35 @@ function createBackground() {
 		}
 	];
 
-	preload.loadManifest(manifest);
+	preloadAssets(manifest, backgroundImageLoaded, backgroundInitialized);
 
 }
 
-function backgroundFileLoaded (event) {
+function backgroundImageLoaded (event) {
 
 	var result = preload.getResult(event.item.id);
 	loadedItem = new createjs.Bitmap(result);
-	
+
 	if (event.item.id == "headerButton") {
 
-		var buttonHome = createTextButton(loadedItem, "Home");
-		buttonHome.x = 150;
+		uiElements.buttonHome = createTextButton(loadedItem, "Home");
+		uiElements.buttonHome.x = 150;
 		var shape = new createjs.Shape ();
-		shape.graphics.rect(buttonHome.x, buttonHome.y, buttonHome.getBounds().width, buttonHome.getBounds().height);
+		shape.graphics.rect(uiElements.buttonHome.x, uiElements.buttonHome.y, uiElements.buttonHome.getBounds().width, uiElements.buttonHome.getBounds().height);
 		shape.cursor = "pointer";
 
-		var buttonHeroes = createTextButton(loadedItem, "Heroes");
-		buttonHeroes.x = 300;
-		var buttonJourney = createTextButton(loadedItem, "Journey");
-		buttonJourney.x = 445;
+		uiElements.buttonHeroes = createTextButton(loadedItem, "Heroes");
+		uiElements.buttonHeroes.x = 300;
+		uiElements.buttonJourney = createTextButton(loadedItem, "Journey");
+		uiElements.buttonJourney.x = 445;
 		
-		layoutContainer.addChild(buttonHome, buttonHeroes, buttonJourney, shape);
-		buttonHome.mouseChildren = false;
-		buttonHome.cursor = "pointer";
+		layoutContainer.addChild(uiElements.buttonHome, uiElements.buttonHeroes, uiElements.buttonJourney);
+	}
+	else if (event.item.id == "contentArea") {
+		uiElements[event.item.id] = loadedItem;
+		loadedItem.x = ($(window).width() - loadedItem.getBounds().width) / 2;
+		loadedItem.y = 150;
+		layoutContainer.addChild(loadedItem);
 	}
 	else {
 		uiElements[event.item.id] = loadedItem;
@@ -97,26 +131,59 @@ function backgroundFileLoaded (event) {
 	}
 }
 
-function backgroundLoaded () {
-	stage.addChild(layoutContainer);
-	resizeLayout();
-}
-
+//Text Button
 function createTextButton (buttonObject, text) {
 
 	var buttonContainer = new createjs.Container();
 	var btnText = new createjs.Text();
 	btnText.text = text;
-	btnText.font = "18px Slabo 27px";
+	btnText.font = "18px 'Slabo 27px'";
 	btnText.color = "white";
 	btnText.x = (buttonObject.getBounds().width - btnText.getBounds().width) / 2;
 	btnText.y = (buttonObject.getBounds().height - btnText.getBounds().height) / 2;
 
 	buttonContainer.addChild(buttonObject, btnText);
-
 	buttonContainer.mouseChildren = false;
-	//buttonContainer.cursor = "pointer";
+
+	var shape = new createjs.Shape ();
+	shape.graphics.beginFill("#000000").drawRect(0,0,buttonContainer.getBounds().width, buttonContainer.getBounds().height);
+	buttonContainer.hitArea = shape;
+	buttonContainer.cursor = "pointer";
 	buttonContainer.cache(0,0,150,60);
 
 	return buttonContainer;
+}
+
+function backgroundInitialized () {
+	stage.addChild(layoutContainer);
+	resizeLayout();
+
+	uiElements.buttonHeroes.on("click", function () {
+		var manifest = [
+			{
+				id: "heroSelection",
+				src: "heroes-selection-bg.jpg"
+			}
+		];
+		preloadAssets(manifest, function (event) {
+
+			layoutContainer.removeChild(uiElements.contentArea);
+
+			var result = preload.getResult(event.item.id);
+			loadedItem = new createjs.Bitmap(result);
+
+			var heroSel1 = loadedItem;
+			var heroSel2 = loadedItem.clone();
+			heroSel2.x = 350;
+			var heroSel3 = loadedItem.clone();
+			heroSel3.x = 700;
+
+			var heroContainer = new createjs.Container();
+			heroContainer.addChild(heroSel1, heroSel2, heroSel3);
+			heroContainer.x = ($(window).width() - heroContainer.getBounds().width) / 2;
+			heroContainer.y = 150;
+
+			stage.addChild(heroContainer);
+		});
+	});
 }
